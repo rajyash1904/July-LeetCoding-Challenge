ad3baed931d2148f6158dd7043e12eff9592e5a8
Yashraj Balidani <ybalidani19.yr@gmail.com>
2020-07-02 14:17:23 +0530

    Day2 Binary Tree Level Order Traversal II

---
+++ Week 1/Day2. Binary_Tree_Level_Order_Traversal_II.cpp
@@ -0,0 +1,60 @@
+/*
+
+Binary Tree Level Order Traversal II
+
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) 
+    {
+        vector<vector<int>> sol;
+        if(root == NULL) return sol;
+        queue<TreeNode*> q;
+        q.push(root);
+        int len=0;
+        while(!q.empty())
+        {
+            len=q.size();
+            vector<int> curr;
+            while(len)
+            {
+                TreeNode* t=q.front();
+                q.pop();
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+                curr.push_back(t->val);
+                len--;
+            }
+            sol.insert(sol.begin(),curr);
+        }
+        return sol;
+    }
+};
